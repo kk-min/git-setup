faf51c31c7de8e2ed387e822ef8ea07346a572af
kk-min <76023265+kk-min@users.noreply.github.com>
2023-04-09 16:33:29 +0800

    Add read_config() function

--- get_repos.py
+++ get_repos.py
@@ -1,4 +1,14 @@
 import requests
+import yaml
+
+def read_config():
+    with open('config.yaml') as config_file:
+        try:
+            config = yaml.safe_load(config_file)
+        except:
+            print('Error reading config.yaml')
+            exit(1)
+    return config
 
 def get_repos(user_name):
     url = 'https://api.github.com/user/repos'
