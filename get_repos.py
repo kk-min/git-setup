d83d527254dc956c9502b7f7699d5461ee510c34
kk-min <76023265+kk-min@users.noreply.github.com>
2023-04-09 16:53:39 +0800

    Replace PyYAML with tomllib

--- get_repos.py
+++ get_repos.py
@@ -1,23 +1,21 @@
 import requests
-import yaml
+import tomllib
 
 def read_config():
-    with open('config.yaml') as config_file:
-        try:
-            config = yaml.safe_load(config_file)
-        except:
-            print('Error reading config.yaml')
-            exit(1)
-    return config
+    with open('config.toml', "rb") as f:
+        cfg = tomllib.load(f)
+    return cfg 
 
-def get_repos(user_name):
+config = read_config()
+
+def get_repos():
     url = 'https://api.github.com/user/repos'
     headers = {
-            "Authorization": f"token {token}",
+            "Authorization": f"token {config['token']}",
             "Accept": "application/vnd.github.v3+json"
     }
     res = requests.get(url, headers=headers)
-    repos = response.json()
+    repos = res.json()
     return repos
 
-
+print(get_repos())
