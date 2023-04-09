6f334ce16ea66c408feeb70d2b22b0feb77a9daa
kk-min <76023265+kk-min@users.noreply.github.com>
2023-04-09 13:39:20 +0800

    Add header to repo get request

--- get_repos.py
+++ get_repos.py
@@ -2,7 +2,11 @@ import requests
 
 def get_repos(user_name):
     url = 'https://api.github.com/user/repos'
-    res = requests.get(url)
+    headers = {
+            "Authorization": f"token {token}",
+            "Accept": "application/vnd.github.v3+json"
+            }
+    res = requests.get(url, headers=headers)
     repos = response.json()
     return repos
 
