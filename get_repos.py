e9b4e7bdd6667575a46bb04f3a58ee6d9c6a8d48
kk-min <76023265+kk-min@users.noreply.github.com>
2023-04-09 16:24:27 +0800

    Cleanup

--- get_repos.py
+++ get_repos.py
@@ -5,7 +5,7 @@ def get_repos(user_name):
     headers = {
             "Authorization": f"token {token}",
             "Accept": "application/vnd.github.v3+json"
-            }
+    }
     res = requests.get(url, headers=headers)
     repos = response.json()
     return repos
