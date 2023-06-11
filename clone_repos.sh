35f79932f043d93eb7242925a93be45cef358b3a
kk-min <76023265+kk-min@users.noreply.github.com>
2023-06-11 21:58:30 +0800

    Add cd commands and validation

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 eval `ssh-agent`
+curr_dir=$(pwd)
 token=$(grep -oP "(?<=\")(.*)(?=\")" config.toml)
+cd $1
+if [[ $? -ne 0 ]]; then
+	echo "Error: $1 is not a valid directory; ensure that target directory exists"
+	exit 1
+fi]]
 curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" https://api.github.com/user/repos | jq -r '.[] | .ssh_url' | while read url; do git clone $url $1; done
+cd $curr_dir
