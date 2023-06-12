e9f944dc0941bf84150af6f079548fb7c567a29f
kk-min <76023265+kk-min@users.noreply.github.com>
2023-06-12 08:42:44 +0800

    Add arg count validation in bash script

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [[ $# -ne 1 ]]; then
+	echo "Usage: ./clone_repos.sh <path>"
+	exit 1
+fi
 eval `ssh-agent`
 curr_dir=$(pwd)
 token=$(grep -oP "(?<=\")(.*)(?=\")" config.toml)
@@ -6,6 +10,6 @@ cd $1
 if [[ $? -ne 0 ]]; then
 	echo "Error: $1 is not a valid directory; ensure that target directory exists"
 	exit 1
-fi]]
+fi
 curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" https://api.github.com/user/repos | jq -r '.[] | .ssh_url' | while read url; do git clone $url; done
 cd $curr_dir
